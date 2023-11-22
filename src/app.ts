891d209da38cd2fecee1b2811a4fba1479377d8a
mdYeasin-cmd <yeasin200011@gmail.com>
2023-11-22 21:31:35 +0600

    student feature done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
 
 const app: Application = express();
 
@@ -7,11 +8,14 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use("/api/v1/students", StudentRoutes);
+
+app.get("/", (req: Request, res: Response) => {
   // let b;
 
-  const a = 10;
-  res.send(a);
+  // const a = 10;
+  res.send("Hello");
 });
 
 export default app;
